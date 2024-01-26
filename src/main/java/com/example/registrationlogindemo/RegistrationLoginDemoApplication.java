1d4cd84809919a40463f75d6b6a397836465a586
AntonioSalinasGarcía <antonio.salinas@ext.cesurformacion.com>
2024-01-26 12:22:03 +0100

    Subir ficheros

--- src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
+++ src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.registrationlogindemo;
 
+import com.example.registrationlogindemo.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class RegistrationLoginDemoApplication {
 
 	public static void main(String[] args) {
